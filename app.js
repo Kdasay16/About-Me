dba3e87681dba861b2667d7654b0107350be8e3b
Kayla Asay <kdasay16@gmail.com>
2017-02-08 15:54:21 -0800

    Commit Three

--- app.js
+++ app.js
@@ -16,19 +16,35 @@ if (quizResponseOne.toUpperCase() === 'Y' || quizResponseOne.toUpperCase() === '
   console.log('Yay you were right!');
 }
 
-var quizResponseTwo = promt('Do I like to boogy all night long?');
+var quizResponseTwo = prompt('Do I like to boogy all night long?');
 
-if (quizResponseTwo.toUpperCase() === 'y' || quizResponseTwo.toUpperCase() === 'YES') {
+if (quizResponseTwo.toUpperCase() === 'Y' || quizResponseTwo.toUpperCase() === 'YES') {
   console.log('Nope! I prefer sleeping.');
 } else {
   console.log('You were right! I rather sleep.');
 }
 
-var quizResponseThree = promt('Does Kayla like the color Aqua?');
+var quizResponseThree = prompt('Does Kayla like the color Aqua?');
 
-if (quizResponseThree.toUpperCase() === 'y' || quizResponseThree.toUpperCase() === 'YES') {
-  console.log('');
+if (quizResponseThree.toUpperCase() === 'Y' || quizResponseThree.toUpperCase() === 'YES') {
+  console.log('You are correct!');
 }
 else {
-  cosole.log('');
+  console.log('Well I do, so you are wrong.');
+}
+
+var quizResponseFour = prompt('Can I leg press 450 lbs?');
+
+if (quizResponseFour.toUpperCase() === 'Y' || quizResponseFour.toUpperCase() === 'YES') {
+  console.log('You betcha I can!');
+} else {
+  console.log('I can. You underestimate my strength.');
+}
+
+var quizResponseFive = prompt('Do I love veggies?');
+
+if (quizResponseFive.toUpperCase() === 'Y' || quizResponseFive.toUpperCase() === 'YES') {
+  console.log('I cannot believe you think I would love veggies. Disappointed.');
+} else {
+  console.log('You are right! Veggies are pretty gross!');
 }
