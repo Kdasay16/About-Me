7777466cd2f8297d38bf20bd0bf788c305ad5148
Kayla Asay <kdasay16@gmail.com>
2017-02-13 10:13:46 -0800

    continuting to change

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 var responseOne = confirm('Hello, would you like to take a quiz?');
 
 if (responseOne) {
-  console.log('Awseome! Lets get started!');
+  alert('Awseome! Lets get started!');
 } else {
-  console.log('Too bad. Starting anyway.');
+  alert('Too bad. Starting anyway.');
 }
 
 allQuestions();
@@ -15,9 +15,9 @@ function allQuestions () {
   var quizResponseOne = prompt('Hi my name is Kayla and I am working on my coding skills. Please answer questions with Y or N. Do I have a fish?');
 
   if (quizResponseOne.toUpperCase() === 'Y' || quizResponseOne.toUpperCase() === 'YES') {
-    console.log('Sorry you were wrong. No fish.');
+    alert('Sorry you were wrong. No fish.');
   } else if(quizResponseOne.toUpperCase() === 'N' || quizResponseOne.toUpperCase() === 'NO') {
-    console.log('Yay you were right!');
+    alert('Yay you were right!');
   } else {
     alert('Please respond with a \'Y\' or \'N\'');
   }
@@ -26,44 +26,44 @@ function allQuestions () {
   var quizResponseTwo = prompt('Do I like to boogy all night long?');
 
   if (quizResponseTwo.toUpperCase() === 'Y' || quizResponseTwo.toUpperCase() === 'YES') {
-    console.log('Nope! I prefer sleeping.');
+    alert('Nope! I prefer sleeping.');
   } else if(quizResponseTwo.toUpperCase() === 'N' || quizResponseTwo.toUpperCase() === 'NO'){
-    console.log('You were right! I rather sleep.');
+    alert('You were right! I rather sleep.');
   } else {
-    console.log('Please respond with a \'Y\' or \'N\'');
+    alert('Please respond with a \'Y\' or \'N\'');
   }
 
   //Question 3
   var quizResponseThree = prompt('Am I addicted to candy cane chapstick?');
 
   if (quizResponseThree.toUpperCase() === 'Y' || quizResponseThree.toUpperCase() === 'YES') {
-    console.log('I am. I really am...');
+    alert('I am. I really am...');
   } else if(quizResponseThree.toUpperCase() === 'N' || quizResponseThree.toUpperCase() === 'NO') {
-    console.log('Well I am, so you are wrong.');
+    alert('Well I am, so you are wrong.');
   } else {
-    console.log('Please respond with a \'Y\' or \'N\'');
+    alert('Please respond with a \'Y\' or \'N\'');
   }
 
   //Question quiz
   var quizResponseFour = prompt('Can I leg press 450 lbs?');
 
   if (quizResponseFour.toUpperCase() === 'Y' || quizResponseFour.toUpperCase() === 'YES') {
-    console.log('You betcha I can!');
+    alert('You betcha I can!');
   } else if(quizResponseFour.toUpperCase() === 'N' || quizResponseFour.toUpperCase() === 'NO') {
-    console.log('I can. You underestimate my strength.');
+    alert('I can. You underestimate my strength.');
   } else {
-    console.log('Please respond with a \'Y\' or \'N\'');
+    alert('Please respond with a \'Y\' or \'N\'');
   }
 
   //Question 5
   var quizResponseFive = prompt('Do I love veggies?');
 
   if (quizResponseFive.toUpperCase() === 'Y' || quizResponseFive.toUpperCase() === 'YES') {
-    console.log('I cannot believe you think I would love veggies. Disappointed.');
+    alert('I cannot believe you think I would love veggies. Disappointed.');
   } else if(quizResponseFive.toUpperCase() === 'N' || quizResponseFive.toUpperCase() === 'NO') {
-    console.log('You are right! Veggies are pretty gross!');
+    alert('You are right! Veggies are pretty gross!');
   } else {
-    console.log('Please respond with a \'Y\' or \'N\'');
+    alert('Please respond with a \'Y\' or \'N\'');
   }
 };
 
@@ -87,8 +87,9 @@ for (var i = 0; i < numAttempts; i++) {
 }
 
 //Question 7
+var questionCount;
 if (questionCount === 6) {
-  alert('Bonus question! You get six tries. What other states have I lived in?');
+  prompt('Bonus question! You get six tries. What other states have I lived in?');
   allStates();
 }
 function allStates() {
@@ -99,7 +100,7 @@ function allStates() {
 
   for (var statesCount = 0; statesCount < numGuesses; statesCount++) {
     var statesQuestion = prompt('Kayla has lived in: ');
-    for (i = 0, i < statesLivedIn.length; i++) {
+    for (i = 0; i < statesLivedIn.length; i++) {
       if (statesQuestion.toUpperCase() === statesLivedIn[i].toUpperCase()) {
         statesComplete = true;
         statesLivedIn.splice(i, 1);
